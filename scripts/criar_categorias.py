8e7dfc99eef7b077801923b378ce397a68fea114
willianjonhy05 <willianjohny05@gmail.com>
2026-03-06 12:46:26 -0300

    Deus do Céu, ainda tem muita coisa

---
+++ scripts/criar_categorias.py
@@ -0,0 +1,19 @@
+from tesouros_do_ifpi.models import Categoria
+
+categorias = [
+    ("Ex-Aluno", "Posição no Mercado de Trabalho; Reconhecimento Social."),
+    ("Aluno Atual", "Desempenho no Curso; Participação em Eventos e Pesquisas; Estar no último período do Curso."),
+    ("Ex-Professor", "Ações que lhe deram destaque na trajetória da Instituição; Dinâmica de Ensino memorável."),
+    ("Professor Atual", "Ações Inovadoras; Participação em Projetos e Extensão."),
+    ("Ex-Técnico", "Proatividade; Marcas das suas ações na história da Instituição."),
+    ("Técnico Atual", "Proatividade; Empatia."),
+    ("Terceirizado", "Eficiência; Socialização."),
+]
+
+for nome, descricao in categorias:
+    Categoria.objects.get_or_create(
+        nome=nome,
+        defaults={"descricao": descricao}
+    )
+
+print("Categorias cadastradas com sucesso!")
